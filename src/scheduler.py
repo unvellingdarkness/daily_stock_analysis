1e972e55472260b634ab87285c3b6114079c18f2
unvellingdarkness <lans304@gmail.com>
2026-02-05 16:00:37 +0800

    Change default schedule time to '10:30'

--- src/scheduler.py
+++ src/scheduler.py
@@ -152,7 +152,7 @@ class Scheduler:
 
 def run_with_schedule(
     task: Callable,
-    schedule_time: str = "22:00",
+    schedule_time: str = "10:30",
     run_immediately: bool = True
 ):
     """
